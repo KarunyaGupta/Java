7195cd0d5c0b47699ec20194ab5d7a8f08ebafc6
KarunyaGupta <mails.karunyagupta@gmail.com>
2024-12-06 10:52:11 +0530

    update files and folder:

---
+++ Basics/TypeCasting.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class TypeCasting{
+    public static void main(String[] args){
+        float a = 10f;
+        int alpha = (int)(a);
+        double beta = (double)(a);
+        System.out.println(alpha);
+        System.out.println(beta);
+
+
+        
+    }
+}
