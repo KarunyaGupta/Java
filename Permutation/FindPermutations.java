22174114f711ae149a633088643b63eafec7b8a9
KarunyaGupta <mails.karunyagupta@gmail.com>
2025-02-03 18:35:43 +0530

    3/2/25

---
+++ Permutation/FindPermutations.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class FindPermutations{
+    public static void main(String[] args){
+        String str = "abc";
+        findPermutation(str,"");++
+
+    }
+    public static void findPermutation(String str , String ans){
+        // basecase
+        if(str.length()==0){
+            System.out.println(ans);
+            return;
+        }
+        for(int i=0;i<str.length();i++){
+            char ch = str.charAt(i);
+            String NewStr = str.substring(0,i) + str.substring(i+1,str.length());  // i doesnt count
+            findPermutation(NewStr,ans+ch);
+        }
+    }
+}
