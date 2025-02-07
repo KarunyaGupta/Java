d59fdeb37f6203f9135bd5f2398bcb7617555724
KarunyaGupta <mails.karunyagupta@gmail.com>
2025-02-07 20:15:10 +0530

    7/2/25

--- BackTracking/NQueenArrayList.java
+++ BackTracking/NQueenArrayList.java
@@ -14,7 +14,7 @@ class NQueen {
         }
         
         nQueen(board, 0, res);
-        
+         
         // Print all solutions in the desired format
         System.out.println(res);
     }
