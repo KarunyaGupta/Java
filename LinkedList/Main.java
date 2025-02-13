12a8374fd019419530a26a4e6a4d0b300a7a150f
KarunyaGupta <mails.karunyagupta@gmail.com>
2025-02-13 13:42:46 +0530

    13/2/25

---
+++ LinkedList/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Main{
+    public static class Node{
+    int data;
+    Node next; // reference variable to next variable
+
+    public Node(int data){ // constructor
+        this.data = data;
+        this.next = null;
+    }
+}
+    public static void main(String[] args){
+
+    }
+}
