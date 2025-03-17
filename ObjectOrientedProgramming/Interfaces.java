3ffbb7e280de298718f1fed7b42f44c834b0c68d
KarunyaGupta <mails.karunyagupta@gmail.com>
2025-03-17 18:46:59 +0530

    interface 17/3/25

--- ObjectOrientedProgramming/Interfaces.java
+++ ObjectOrientedProgramming/Interfaces.java
@@ -6,7 +6,28 @@
 // used to achieve total abstraction 
 // variable in the interface are final , public and static 
 
-
+interface MyInterface{
+    int a = 10;
+    void display();
+    void show();
+    void print();
+}
 class Interfaces{
     
+    public static void main(String[] args){
+        MyInterface obj = new MyInterface(){
+            public void display(){
+                System.out.println("Display");
+            }
+            public void show(){
+                System.out.println("Show");
+            }
+            public void print(){
+                System.out.println("Print");
+            }
+        };
+        obj.display();
+        obj.show();
+        obj.print();
+    }
 }
