727f8ebce8cc8878166d79b8d27cdaae829fe54b
KarunyaGupta <mails.karunyagupta@gmail.com>
2025-03-30 14:26:52 +0530

    aere

--- SortingAlgoritm/MergeSort.java
+++ SortingAlgoritm/MergeSort.java
@@ -1,4 +1,4 @@
-!import java.util.*;
+import java.util.*;
 
 class MergeSort {
     public static void main(String[] args) {
@@ -55,4 +55,22 @@ class MergeSort {
     public static void printArray(int[] arr) {
         System.out.print(Arrays.toString(arr));
     }
+
+    // public static void mergeSort(int[] arr , int start , int end){
+    //     if(start>=end){
+    //         return;
+    //     }
+    //     int mid = start + (end-start)/2;
+    //     mergeSort(arr,start , mid);
+    //     mergeSort(arr,mid+1,end);
+    //     merge(arr, start , mid , end);
+    // }
+
+    // public static void merge(int[] arr , int start , int mid , int end){
+    //     int i = start;
+    //     int j = mid + 1;
+    //     int k = 0;
+
+
+    // }
 }
