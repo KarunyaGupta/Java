e448ee4c379d9cc81263269b0f96b018900c860d
KarunyaGupta <mails.karunyagupta@gmail.com>
2025-04-14 16:35:34 +0530

    reverse Stack

--- Stack/ReverseString.java
+++ Stack/ReverseString.java
@@ -1,29 +1,18 @@
 import java.util.Stack;
+class ReverseString{
+    public static void main(String[] args){
+        String str = "Karunya Gupta";
+        Stack<Character> stack = new Stack<>();
+        for(int i=0;i<str.length();i++){
+            stack.push(str.charAt(i));
+        }
 
-class ReverseString {
-    String str = "Karunya Gupta";
-
-    public static String reverse(String str) {
-        Stack<Character> s = new Stack<>();
         StringBuilder reversed = new StringBuilder();
 
-        // Push all characters onto stack
-        for(char c : str.toCharArray()) {
-            s.push(c);
+        while(!stack.isEmpty()){
+            reversed.append(stack.pop());
         }
 
-        // Pop all characters to create reversed string
-        while(!s.isEmpty()) {
-            reversed.append(s.pop());
-        }
-
-        return reversed.toString();
-    }
-
-    public static void main(String[] args) {
-        ReverseString rs = new ReverseString();
-        String result = reverse(rs.str);
-        System.out.println("Original string: " + rs.str);
-        System.out.println("Reversed string: " + result);
+        System.out.println(reversed.toString());
     }
 }
